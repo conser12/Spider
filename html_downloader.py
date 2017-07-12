414a16742aaafbb1604f7275efb625def7f5f10a
chen.peijun <chen.peijun@ztesoft.com>
2017-07-12 23:05:37 +0800

    Revert "将python2.7修改为3.6版本"
    This reverts commit 8b3a05bb0bbec606a926d4d2ef06385eb8f03ab2.

--- html_downloader.py
+++ html_downloader.py
@@ -1,12 +1,12 @@
 
-import urllib.request
+import urllib2
 
 class HtmlDownloader(object):
     def download(self, url):
         if url is None:
             return None
 
-        response = urllib.request.urlopen(url)
+        response = urllib2.urlopen(url)
 
         if response.getcode() != 200:
             return None
--- html_outputer.py
+++ html_outputer.py
@@ -10,22 +10,17 @@ class HtmlOutputer(object):
         self.datas.append(data)
 
     def output_html(self):
-        fout = open('output.html', 'w', encoding='utf-8')
+        fout = open('output.html', 'w')
 
         fout.write("<html>")
-        #当把抓取出来的内容显示在网页上时，这个地方是出错的关键点。在windows下面，新文件的默认编码是gbk，
-        # 这样的话，python解释器会用gbk编码去解析我们的网络数据流txt，然而txt此时已经是decode过的unicode编码，
-        # 这样的话就会导致解析不了，出现问题。应写为： fout = open('output.html','w',encoding='utf-8') 同时在html中声明<meta charset='utf-8'>
-        fout.write('<meta charset=\'utf-8\'>')
         fout.write("<body>")
         fout.write("<table>")
 
         for data in self.datas:
             fout.write("<tr>")
             fout.write("<td>%s</td>" % data['url'])
-            #无需.encode('utf8')去掉,即可解决mac乱码问题,因为mac(Linux)是默认utf8编码,而Windows默认是gbk,
-            fout.write("<td>%s</td>" % data['title'])
-            fout.write("<td>%s</td>" % data['summary'])
+            fout.write("<td>%s</td>" % data['title'].encode('utf-8'))
+            fout.write("<td>%s</td>" % data['summary'].encode('utf-8'))
             fout.write("</tr>")
 
         fout.write("</table>")
